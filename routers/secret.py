fbc8342ee0f7ecd77049ed83d442785885238303
ren <denis30510@gmail.com>
2023-11-22 15:02:39 +0300

    created post secret_key

--- routers/secret.py
+++ routers/secret.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from models.secretin import Secret
 from config.db import collection_name
-from starlette.requests import Request
 from schemas.secret import secretEntity
 from service.service import random_secret_key
 
@@ -21,3 +20,10 @@ async def post_secret(secret: str, password: str):
     id = collection_name.insert_one((dict(new_secret))).inserted_id
     return secretEntity(collection_name.find_one({'_id': id}))['secret_key']
 
+
+@router.post('/secrets/{secret_key}')
+async def get_secret(password: str, secret_key: str):
+    secret = collection_name.find_one({'password': password, 'secret_key': secret_key})
+    if secret is not None:
+        return secretEntity(secret)['secret']
+    return secret
